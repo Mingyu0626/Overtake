e4863d5e6ad653895ac68b5fa6a952bcd1a9c5ae
Mingyu0626 <mg010626@naver.com>
2024-03-24 20:52:25 +0900

    Feat | TitleScene 오브젝트 페이드 구현

---
+++ Assets/Scripts/SceneManager/TitleSceneManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public float Speed = 0.1f;
+    public TextMeshProUGUI ProducedBy;
+    public TextMeshProUGUI Overtake;
+    public TextMeshProUGUI Motto;
+    public Image BG;
+
+    [SerializeField]
+    [Range(0.01f, 10f)]
+    private float FadeTime;
+    private bool isProduceByBright = false;
+    private bool isOvertakeReady = false;
+    public void ChangeScene(int SceneNumber)
+    {
+        switch(SceneNumber)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+
+    private IEnumerator Fade(float start, float end, TextMeshProUGUI tmp)
+    {
+        float CurrentTime = 0.0f;
+        float Percent = 0.0f;
+        while (Percent < 1)
+        {
+            CurrentTime += Time.deltaTime;
+            Percent = CurrentTime / FadeTime;
+
+            Color color = tmp.color;
+            color.a = Mathf.Lerp(start, end, Percent);
+            tmp.color = color;
+            yield return null;
+        }
+
+        if (tmp.color.a == 1) isProduceByBright = true;
+        else
+        {
+            isOvertakeReady = true;
+            ProducedBy.gameObject.SetActive(false);
+        }
+    }
+
+
+    private void Awake()
+    {
+        StartCoroutine(Fade(0, 1, ProducedBy));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isProduceByBright) 
+        {
+            StartCoroutine(Fade(1, 0, ProducedBy));
+        }
+        if (isOvertakeReady)
+        {
+            StartCoroutine(Fade(0, 1, Overtake));
+            StartCoroutine(Fade(0, 1, Motto));
+        }
+    }
+}
