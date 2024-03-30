ac94ef976d18d482b002f9cfaa57159895817d6b
Mingyu0626 <mg010626@naver.com>
2024-03-31 01:01:29 +0900

    Feat | 씬전환 슬라이드 애니메이션 구현

---
+++ Assets/Scripts/Animation/ChangeSceneAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneAnimation : MonoBehaviour
+{
+
+    public GameObject Left;
+    public GameObject Right;
+    public bool IsAnimationOpened = false;
+
+    public void OpenSceneAnimation()
+    {
+        LeanTween.moveX(Left, Left.transform.position.x - 10f, 1f).setEaseOutQuad();
+        LeanTween.moveX(Right, Right.transform.position.x + 10f, 1f).setEaseOutQuad().setOnComplete(OnAnimationComplete);
+    }
+
+    public void CloseSceneAnimation()
+    {
+        LeanTween.moveX(Left, Left.transform.position.x + 10f, 1f).setEaseOutQuad();
+        LeanTween.moveX(Right, Right.transform.position.x - 10f, 1f).setEaseOutQuad().setOnComplete(OnAnimationComplete);
+    }
+
+    private void OnAnimationComplete()
+    {
+        Debug.Log("OpenSceneAnimation or CloseSceneAnimation Complete");
+        if (!IsAnimationOpened) IsAnimationOpened = true;
+        else IsAnimationOpened = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenSceneAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TitleScene/ButtonManager.cs
+++ Assets/Scripts/TitleScene/ButtonManager.cs
@@ -6,14 +6,15 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     [SerializeField] private GameObject SettingUI;
+    private ChangeSceneAnimation ChangeSceneAnimationScript;
     public void GoSelectMusicScene()
     {
-        SceneManager.LoadScene("SelectMusicScene");
+        CloseAnimation("SelectMusicScene");
     }
 
     public void GoSkillAnalyzerScene()
     {
-        SceneManager.LoadScene("SkillAnalyzerScene");
+        CloseAnimation("SkillAnalyzerScene");
     }
     public void OnSettingMenu()
     {
@@ -21,10 +22,35 @@ public class ButtonManager : MonoBehaviour
 
     }
 
+    private void CloseAnimation(string SceneName)
+    {
+        if (ChangeSceneAnimationScript != null)
+        {
+            ChangeSceneAnimationScript.CloseSceneAnimation();
+
+            // CloseAnimation()이 실행된 후 1초 뒤에 체크 수행
+            LeanTween.delayedCall(1.1f, () =>
+            {
+                if (CheckCompletelyClosed()) SceneManager.LoadScene(SceneName);
+            });
+        }
+
+        else
+        {
+            Debug.LogError("ChangeSceneAnimation 스크립트를 찾을 수 없습니다.");
+        }
+    }
+
+    private bool CheckCompletelyClosed()
+    {
+        if (ChangeSceneAnimationScript != null && !ChangeSceneAnimationScript.IsAnimationOpened) return true;
+        else return false;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        ChangeSceneAnimationScript = GetComponent<ChangeSceneAnimation>();
         SettingUI.SetActive(false);
     }
 
