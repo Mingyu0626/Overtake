599bda13392388f3c81e569075a34c703c99ef49
Mingyu0626 <mg010626@naver.com>
2024-04-07 18:15:31 +0900

    Feat | SelectMusicScene 매니저 추가 및 구현중

---
+++ Assets/Scripts/SelectMusicScene/SelectMusicManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Music
+{
+    public string title;
+    public string composer;
+    public int bpm;
+    public Sprite sprite;
+}
+
+public class SelectMusicSceneManager : MonoBehaviour
+{
+    [SerializeField] Music[] MusicList = null;
+    [SerializeField] Text TextMusicTitle = null;
+    [SerializeField] Text TextMusicComposer = null;
+    [SerializeField] Text TextMusicBPM = null;
+    [SerializeField] Image Img = null;
+    [SerializeField] GameObject TitleMenu = null;
+
+    int CurrentMusic = 0;
+
+    public void ListUp()
+    {
+        CurrentMusic--;
+        if (CurrentMusic < 0) CurrentMusic = MusicList.Length - 1;
+    }
+
+    public void ListDown()
+    {
+        CurrentMusic++;
+        if (CurrentMusic >= MusicList.Length - 1) CurrentMusic = 0;
+    }
+
+    public void SetMusicInfo()
+    {
+        TextMusicTitle.text = MusicList[CurrentMusic].title;
+        TextMusicComposer.text = MusicList[CurrentMusic].composer;
+        TextMusicBPM.text = MusicList[CurrentMusic].bpm.ToString();
+        Img.sprite = MusicList[CurrentMusic].sprite;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
